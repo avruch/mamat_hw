ed3816983a8d4740cdfbb4f1a22d87d84128de02
avruch <yuvalavr@gmail.com>
2021-04-19 19:35:18 +0300

    final

--- HW2/course_stat.sh
+++ HW2/course_stat.sh
@@ -1,22 +1,15 @@
  #!/bin/bash 
-
-read input
-
-number=$(echo -n $input | wc -c )
+number=$(echo -n $1 | wc -c )
 if   [[ $number != 6 ]] ; then 
  	echo "Wrong number of arguments" >&2
  	
-elif [[ !(-f $input".txt") ]];then
+elif [[ !(-f $1".txt") ]];then
 		echo "Course not found" >&2
 	else 
 		#remove the directory if exists 
-		rm -rf $input"_stat" #&> /dev/null
-		mkdir $input"_stat" 
- 		awk '{if (NR!=1) {print $2}}' $input".txt"> ./$input"_stat"/grades.txt 
+		rm -rf $1"_stat" 
+		mkdir $1"_stat" 
+ 		awk '{if (NR!=1) {print $2}}' $1".txt"> ./$1"_stat"/grades.txt 
  		gcc -g -Wall calc_statistics.c -o prog.exe
- 		./prog.exe "./$input"_stat"/grades.txt" > ./$input"_stat"/course_statistics.txt 2> /dev/null
+ 		./prog.exe "./$1"_stat"/grades.txt" > ./$1"_stat"/course_statistics.txt 2> /dev/null
  fi
-
-
-
-#&> /dev/null
