0933ea6a18e47ead0e2481970ce2563df4a73bc0
avruch <74109922+avruch@users.noreply.github.com>
2021-06-06 17:16:04 +0300

    Update port.h

--- HW6/port.h
+++ HW6/port.h
@@ -9,7 +9,15 @@ private:
 public:
 	~Port();
     Port(String pattern);
+    /**
+     * @brief put the relvent values to rule.
+     * @return true if success 
+     */
     bool set_value(String val);
+    /**
+     * @brief matching port values to legal range
+     * @return true if the value in range
+     */
     bool match_value(String val) const;
 };
 
