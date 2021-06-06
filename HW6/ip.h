0b338fb861718893e4fbf719675decd0def2eddd
avruch <74109922+avruch@users.noreply.github.com>
2021-06-06 17:12:53 +0300

    Update ip.h

--- HW6/ip.h
+++ HW6/ip.h
@@ -15,7 +15,15 @@ public:
 	~Ip();
 
     Ip(String pattern);
+     /**
+     * @brief put the relvent values to rule.
+     * @return true if success 
+     */
     bool set_value(String val);
+     /**
+     * @brief matching ip values to legal range
+     * @return true if the value in range
+     */
     bool match_value(String val) const;
 };
 
